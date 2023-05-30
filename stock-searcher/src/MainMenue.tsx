a10c177359f0ff9693d923d226ed723cd7fe42d3
Adem Durakovic <adem.durakovic@slu.edu>
2023-05-30 15:02:41 -0500

    inital set up

---
+++ stock-searcher/src/MainMenue.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+interface SearchBoxProps {
+  onSearch: (query: string) => void;
+}
+
+const SearchBox: React.FC<SearchBoxProps> = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onSearch(query);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={query}
+        onChange={handleInputChange}
+        placeholder="Search..."
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+export default SearchBox;
